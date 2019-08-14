5bbbedcdafbdb8f7046bf4130ba3a5f7b50a78dd
Marianne Pyon <mariannepyon@gmail.com>
2019-08-14 13:38:14 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,7 +10,7 @@ class School
   end
 
   def add_student(student_name, grade)
-    @student_name = name
+    @student_name = student_name
     @grade = grade
     if @roster.include?(grade) == false
       @roster[grade] = []
