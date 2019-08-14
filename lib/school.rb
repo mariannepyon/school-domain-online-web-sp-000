c4e80eca974f2e0a747996ccea70c2a0cd466e16
Marianne Pyon <mariannepyon@gmail.com>
2019-08-14 13:14:50 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,44 @@
 # code here!
+class School
+
+ROSTER = []
+
+  def initialize(new)
+    @new = new
+  end
+
+  def new
+    @new
+  end
+
+  def roster= (new)
+    @roster = roster
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student= (new, grade)
+    @add_student = add_student
+    ROSTER << add_student
+  end
+
+  def grade= (grade)
+    @grade = grade
+  end
+
+  def grade
+    @grade
+  end
+
+  def sort
+    @sort = sort
+  end
+
+  def sort
+    @sort
+  end
+
+
+end
